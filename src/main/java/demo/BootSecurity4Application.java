745e7c67bbfd282fd318615141ad699ea56b4677
Rob Winch <rwinch@gopivotal.com>
2015-04-29 12:12:49 -0500

    Initial

---
+++ src/main/java/demo/BootSecurity4Application.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurity4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurity4Application.class, args);
+    }
+}
